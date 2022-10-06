99f82852e88c5a2a0ecbd25a493324970f89d18b
karengcecena <karengcecena@gmail.com>
2022-10-05 17:14:07 -0700

    Task 3 Add Melon to Cart Complete

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -49,17 +49,45 @@ def show_melon(melon_id):
 
     Show all info about a melon. Also, provide a button to buy that melon.
     """
-
+    # code for Isis - changed "meli" to melon_id
     melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
 
 
+@app.route("/add_to_cart/<melon_id>")
+def add_to_cart(melon_id):
+    
+    """Add a melon to cart and redirect to shopping cart page.
+
+    When a melon is added to the cart, redirect browser to the shopping cart
+    page and display a confirmation message: 'Melon successfully added to
+    cart'."""
+    
+    session["cart"] = {}
+
+    if melon_id in session["cart"]:
+        session["cart"][melon_id] += 1
+
+    else:
+        session["cart"][melon_id] = 1
+
+    # - flash a success message
+    flash(f"Success! You have added {melon_id} to the cart.")
+    
+    # - redirect the user to the cart page
+    return redirect("/cart")
+
+
 @app.route("/cart")
 def show_shopping_cart():
     """Display content of shopping cart."""
 
+    melons = request.args(session["cart"])
+    
+
+    
     # TODO: Display the contents of the shopping cart.
 
     # The logic here will be something like:
@@ -79,28 +107,6 @@ def show_shopping_cart():
     # been added to the session
 
     return render_template("cart.html")
-
-
-@app.route("/add_to_cart/<melon_id>")
-def add_to_cart(melon_id):
-    """Add a melon to cart and redirect to shopping cart page.
-
-    When a melon is added to the cart, redirect browser to the shopping cart
-    page and display a confirmation message: 'Melon successfully added to
-    cart'."""
-
-    # TODO: Finish shopping cart functionality
-
-    # The logic here should be something like:
-    #
-    # - check if a "cart" exists in the session, and create one (an empty
-    #   dictionary keyed to the string "cart") if not
-    # - check if the desired melon id is the cart, and if not, put it in
-    # - increment the count for that melon id by 1
-    # - flash a success message
-    # - redirect the user to the cart page
-
-    return "Oops! This needs to be implemented!"
 
 
 @app.route("/login", methods=["GET"])
